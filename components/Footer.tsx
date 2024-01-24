9da7b76bd06ac17a8831f3e22b6a5da0ec479c94
urbotbro <thebigggbro@gmail.com>
2024-01-24 14:16:34 +0100

    Site ready for styling and responsive design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,35 @@
-import { Box, Flex } from "@chakra-ui/react"
+import { Box, Flex, Image } from "@chakra-ui/react";
+import { Link } from '@chakra-ui/next-js';
+import { FaXTwitter } from "react-icons/fa6";
+import { FaTelegram } from "react-icons/fa";
 
 const Footer = () => {
+  const date = new Date();
   return (
-    <Flex as='footer' justify='center' >
-      <Flex as='div' width='100%' maxWidth='1280px' justify='center'>
-        Footer
+    <Flex as='footer' justify='center' direction='column' align='center'>
+      <Flex direction='column' as='div' width='100%' maxWidth='1280px' mt='150px' mb='150px' justify='center' align='center'>
+        <Box width='200px'>
+          <Image src='/Logo.png' alt='Footer Logo' />
+        </Box>
+        <Box mt='50px' fontSize='30px' fontWeight='700'>
+          Start Trading with BRO BOT
+        </Box>
+        
+        <Box>
+          <Flex>
+            <Link href='/#'>
+              <FaXTwitter />
+            </Link>
+            <Link href="/#">
+              <FaTelegram />
+            </Link>
+          </Flex>
+        </Box>
+
       </Flex>
+        <Box fontSize='14px' mb='10px'>
+          Copyright &copy;{date.getFullYear()} BRO BOT. All rights reserved.
+        </Box>
     </Flex>
   )
 }
--- components/Home/Boost.tsx
+++ components/Home/Boost.tsx
@@ -5,16 +5,16 @@ import { Box, Flex, Heading, Image } from "@chakra-ui/react"
 const Boost = () => {
     return (
     <Flex width="100%" maxWidth='1280px' justify='center' mt='150px' mb='150px' align='center'>
-        <Box>
-            <Heading>
+        <Box width='60%'>
+            <Heading  pb='20px'>
                 Trade Like Never Before
             </Heading>
             <Box>
-                Dive into the world of BRO Bot, your ultimate Telegram companion. Experience the adrenaline of sniping the hottest launches or effortlessly acquiring live tokens. Crafted for the Ethereum Network, BRO Bot is not just a tool, it's your gateway to seamless trading. As we evolve, more chains await. Join us, and redefine how you trade. Unleash the power. Don&apos;t just trade, BRO-trade! 🔥
+                Dive into the world of BRO Bot, your ultimate Telegram companion. Experience the adrenaline of sniping the hottest launches or effortlessly acquiring live tokens. Crafted for the Ethereum Network, BRO Bot is not just a tool, it&apos;s your gateway to seamless trading. As we evolve, more chains await. Join us, and redefine how you trade. Unleash the power. Don&apos;t just trade, BRO-trade! 🔥
             </Box>
         </Box>
-        <Box>
-            <Image src="/Buysell.gif" alt="Trade Like Never Before" />
+        <Box width='40%' display='flex' justifyContent='center' alignItems='center'>
+            <Image src="/Buysell.gif"  alt="Trade Like Never Before" width='70%' />
         </Box>
     </Flex>
     )
