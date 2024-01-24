6509588d455a0fc200d480a7e4a2751ffac6140c
urbotbro <thebigggbro@gmail.com>
2024-01-24 15:06:37 +0100

    Working on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import { Link, Image } from '@chakra-ui/next-js';
 
 const Header = () => {
   return (
-    <Flex as='header' justifyContent='center' align='center'>
+    <Flex as='header' justifyContent='center' align='center' background='#14191e' color='white' position='fixed' left='0' right='0' zIndex='1000'>
       <Flex as='div' position='sticky' maxWidth='1280px' width="100%" justify='space-between' align='center' paddingTop='10px' paddingBottom='10px'>
         <Box>
           <Image src='/Logo.png' alt="BRO Bot" width={50} height={50} />
@@ -18,9 +18,11 @@ const Header = () => {
           <Link href='#' pl='10px' pr='10px' fontSize='14px' _hover={{color: ''}}>Docs</Link>
         </Box>
         <Box>
-          <Button>
-            Dashboard
-          </Button>
+          <Link href="https://bro-dashboard-gules.vercel.app/">
+            <Button borderRadius='100px' fontSize='14px' background='#cdf6f8'>
+              Dashboard
+            </Button>
+          </Link>
         </Box>
 
       </Flex>
--- contexts/Chakra-provider.tsx
+++ contexts/Chakra-provider.tsx
@@ -1,5 +1,12 @@
 'use client'
-import { ChakraProvider } from '@chakra-ui/react'
+
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const baseTheme = extendTheme({
+    colors: {
+        
+    }
+})
 
 
 export function Providers({ children }: { children: React.ReactNode }) {
