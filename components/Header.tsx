06d58d6b363b61a26be877a07175068794bc8f62
urbotbro <thebigggbro@gmail.com>
2024-01-26 16:36:29 +0100

    Add new menu curtain

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,19 @@
 'use client';
 import { Box, Button, Flex } from '@chakra-ui/react'
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Image } from '@chakra-ui/next-js';
 import { Ubuntu, Orbitron } from "next/font/google";
+import { FaBars, FaXmark } from 'react-icons/fa6';
+import glitch from '@/components/Home/glitch.module.css'
 
 const ubuntuFont = Ubuntu({weight: ['300', '400', '500', '700'], subsets: ['latin']})
 const orbitronFont = Orbitron({weight: ['400', '500', '600', '700'], subsets: ['latin']})
 
 const Header = () => {
+  
+  const [state, changeState] = useState(false)
+
+
   return (
     <Flex as='header' justifyContent='center' align='center' background='#0c0f12' color='white' position='fixed' top='0' width="100%" zIndex='1000' pl='10px' pr='10px'>
       <Flex as='div' position='sticky' maxWidth='1280px' width="100%" justify='space-between' align='center' paddingTop='20px' paddingBottom='20px' pl='20px' pr='20px'>
@@ -30,6 +36,9 @@ const Header = () => {
             </Button>
           </Link>
         </Box>
+          <Box display={{base: 'flex', md: 'none'}} height="100%" width="27px" className={glitch.menu} justifyContent='center' alignItems='center' onClick={() => {changeState(!state)}}>
+            {state ? <FaXmark /> : <FaBars />}
+          </Box>
 
       </Flex>
     </Flex>
