e6e91c6f3a04628c611512d02632011c9af7aed4
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-02-05 01:32:31 +0600

    Update Intro.tsx
    major changes

--- components/Home/Intro.tsx
+++ components/Home/Intro.tsx
@@ -2,45 +2,47 @@ import { Box, Flex, Text, Image as ChakraImage, Heading, Badge, Tooltip, Link } 
 import { FaTelegram, FaGlobe, FaRocket, FaAppStore, FaGooglePlay, FaTwitter } from "react-icons/fa";
 import { RiTelegramLine } from "react-icons/ri";
 import glitch from "@/components/Home/glitch.module.css";
+
+// Assuming the font imports work correctly with your setup
 import { Ubuntu, Orbitron } from "next/font/google";
+
 const ubuntuFont = Ubuntu({ weight: ['300', '400', '500', '700'], subsets: ['latin'] });
 const orbitronFont = Orbitron({ weight: ['400', '500', '600', '700'], subsets: ['latin'] });
-const Intro = () => {
+
+const HomePage = () => {
   return (
-    <Flex as='section' width='100%' justify='center' align='center'>
-      <Flex as='section' width="100%" direction={{ base: 'column-reverse', md: 'row' }} maxWidth="1280px" justifyContent='center' align="center" minH='700px' pl='20px' pr='20px'>
-        <Box width={{ base: '100%', md: "60%" }} textAlign={{ base: 'center', md: 'start' }} display={{ base: 'flex', md: 'block' }} flexDirection={{ base: 'column' }}>
-          <Heading fontSize={{ base: '33px', md: '40px', lg: '45px', xl: '55px' }} pb='20px' className={glitch.heroContainer} style={orbitronFont.style}>
-            <Text className={`${glitch.glitch} ${glitch.hero} ${glitch.layers}`} data-text="BRO BOT: Your Ultimate Trading Ally">BRO BOT: Your Ultimate Trading Ally</Text>
-          </Heading>
-          <Text fontSize={{ base: '20px', md: '25px' }} mt={{ base: '20px', sm: "0" }} lineHeight={{ base: '160%', sm: 'initial' }} style={orbitronFont.style}>
-            Harness the unmatched power of the BRO BOT to dominate the trading landscape.
-          </Text>
-        </Box>
-        <Box width={{ base: '90%', md: "40%" }} mt={{ base: '100px', md: '0' }} mb={{ base: '20px', md: '0' }} display={{ base: 'none', sm: 'flex' }} justifyContent='center' alignItems='center'>
-          <ChakraImage src='/GIF.gif' alt="BroBot Logo" width="350px" />
+    <>
+      <Flex as='section' width='100%' justify='center' align='center'>
+        <Flex as='section' width="100%" direction={{ base: 'column-reverse', md: 'row' }} maxWidth="1280px" justifyContent='center' align="center" minH='700px' pl='20px' pr='20px'>
+          <Box width={{ base: '100%', md: "60%" }} textAlign={{ base: 'center', md: 'start' }} display={{ base: 'flex', md: 'block' }} flexDirection={{ base: 'column' }}>
+            <Heading fontSize={{ base: '33px', md: '40px', lg: '45px', xl: '55px' }} pb='20px' className={glitch.heroContainer} style={orbitronFont.style}>
+              <Text className={`${glitch.glitch} ${glitch.hero} ${glitch.layers}`} data-text="BRO BOT: Your Ultimate Trading Ally">BRO BOT: Your Ultimate Trading Ally</Text>
+            </Heading>
+            <Text fontSize={{ base: '20px', md: '25px' }} mt={{ base: '20px', sm: "0" }} lineHeight={{ base: '160%', sm: 'initial' }} style={orbitronFont.style}>
+              Harness the unmatched power of the BRO BOT to dominate the trading landscape.
+            </Text>
+          </Box>
+          <Box width={{ base: '90%', md: "40%" }} mt={{ base: '100px', md: '0' }} mb={{ base: '20px', md: '0' }} display={{ base: 'none', sm: 'flex' }} justifyContent='center' alignItems='center'>
+            <ChakraImage src='/GIF.gif' alt="BroBot Logo" width="350px"/>
+          </Box>
+        </Flex>
+        <Box position='absolute' background={`url('/Video.gif') center no-repeat`} left='0' right='0' minH='800px' zIndex='-100' opacity='.1' mt='20px'>
         </Box>
       </Flex>
-      <Box position='absolute' background={`url('/Video.gif') center no-repeat`} left='0' right='0' minH='800px' zIndex='-100' opacity='.1' mt='20px'>
-      </Box>
-    </Flex>
-  );
-};
 
-const Webapp = () => {
-  return (
-    <Box mt={{ base: '100px' }} display='flex' flexDirection='column' alignItems="center" width="100%" maxWidth='1280px' pl='20px' pr='20px'>
-      <Flex alignItems='center' justify={{ base: 'space-evenly', md: 'space-evenly' }} flexWrap='wrap' width='100%'>
-        <Box as={Link} href="/#" flexDirection={{base: 'column', md: 'row'}} justifyContent='center' _hover={{textDecoration:'none'}} display='flex' alignItems='center' mr={{base: '0',md: '15px'}} background='#0088cc' padding='5px' borderRadius='5px'>
-                <Box className={glitch.menu} height="85%" width={{base: '30px', sm: '45px'}} pr={{base: '0', md:'8px'}}>
-                    <FaTelegram />
-                </Box>
-                <Box display={{base: 'flex', md: 'block'}} flexDirection='column' justifyContent='center' alignItems='center' mt={{base: '5px', md: '0'}}>
-                    <Badge fontSize={{base: '8px', sm: '11px'}} p='2px' borderRadius='5px'>Available</Badge>
-                    <Box fontSize={{base: '11px', sm: '14', md: '17px'}} textTransform='uppercase' fontWeight='700' mt={{base: '5px', md: '0'}}>BRO BOT</Box>
-               </Box>
+      <Box mt={{ base: '100px' }} display='flex' flexDirection='column' alignItems="center" width="100%" maxWidth='1280px' pl='20px' pr='20px'>
+        <Flex alignItems='center' justify={{ base: 'space-evenly', md: 'space-evenly' }} flexWrap='wrap' width='100%'>
+          {/* BRO BOT Available */}
+          <Box as={Link} href="/#" flexDirection={{ base: 'column', md: 'row' }} justifyContent='center' _hover={{ textDecoration: 'none' }} display='flex' alignItems='center' mr={{ base: '0', md: '15px' }} background='#0088cc' padding='5px' borderRadius='5px'>
+            <Box className={glitch.menu} height="85%" width={{ base: '30px', sm: '45px' }} pr={{ base: '0', md: '8px' }}>
+              <FaTelegram />
             </Box>
-            <Box as={Link} href="/#" flexDirection={{ base: 'column', md: 'row' }} justifyContent='center' _hover={{ textDecoration: 'none' }} display='flex' alignItems='center' mr={{ base: '0', md: '15px' }} background='#0088cc' padding='5px' borderRadius='5px'>
+            <Box display={{ base: 'flex', md: 'block' }} flexDirection='column' justifyContent='center' alignItems='center' mt={{ base: '5px', md: '0' }}>
+              <Badge fontSize={{ base: '8px', sm: '11px' }} p='2px' borderRadius='5px'>Available</Badge>
+              <Box fontSize={{ base: '11px', sm: '14px', md: '17px' }} textTransform='uppercase' fontWeight='700' mt={{ base: '5px', md: '0' }}>BRO BOT</Box>
+            </Box>
+          </Box>
+          <Box as={Link} href="/#" flexDirection={{ base: 'column', md: 'row' }} justifyContent='center' _hover={{ textDecoration: 'none' }} display='flex' alignItems='center' mr={{ base: '0', md: '15px' }} background='#0088cc' padding='5px' borderRadius='5px'>
                 <Box className={glitch.menu} height="85%" width={{ base: '30px', sm: '45px' }} pr={{ base: '0', md: '8px' }}>
                     <FaTelegram />
                 </Box>
@@ -123,16 +125,10 @@ const Webapp = () => {
             backgroundRepeat: 'no-repeat'
         }}></Box>
         <Box zIndex='11' fontSize='25px' fontWeight='700'>Coming Soon</Box>
-      </Flex>
-    </Box>
+        </Flex>
+      </Box>
+    </>
   );
 };
 
-export default function HomePage() {
-  return (
-    <>
-      <Intro />
-      <Webapp />
-    </>
-  );
-}
+export default HomePage;
