760499718ed545813f5b9f877f90861d1820d680
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-02-04 15:42:04 +0600

    Update Intro.tsx
    add text coorection

--- components/Home/Intro.tsx
+++ components/Home/Intro.tsx
@@ -12,7 +12,7 @@ const Intro = () => {
         <Flex as='section' width="100%" direction={{base: 'column-reverse', md: 'row'}} maxWidth="1280px" justifyContent='center' align="center" minH='700px' pl='20px' pr='20px'>
             <Box width={{base: '100%', md:"60%"}} textAlign={{base: 'center', md: 'start'}} display={{base: 'flex', md: 'block'}} flexDirection={{base: 'column'}}>
                 <Heading fontSize={{base: '33px', md: '40px', lg: '45px', xl: '55px'}} pb='20px' className={newHero.heroContainer} style={orbitronFont.style}>
-                    <Text className={`${newHero.glitch} ${newHero.hero} ${newHero.layers}`} data-text="BRO Bot: Your Ultimate Trading Ally">BRO Bot: Your Ultimate Trading Ally</Text>
+                    <Text className={`${newHero.glitch} ${newHero.hero} ${newHero.layers}`} data-text="BRO BOT: Your Ultimate Trading Ally">BRO Bot: Your Ultimate Trading Ally</Text>
                 </Heading>
                 <Text fontSize={{base: '20px', md: '25px'}} mt={{base: '20px', sm:"0"}} lineHeight={{base: '160%', sm: 'initial'}} style={orbitronFont.style}>
                     Harness the unmatched power of the BRO Bot to dominate the trading landscape
