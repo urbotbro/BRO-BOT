7ceb8e7c50c43f02f66ec19f7a684755d1949835
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-02-04 17:26:07 +0600

    Update Webapp.tsx
    text size

--- components/Home/Webapp.tsx
+++ components/Home/Webapp.tsx
@@ -9,13 +9,13 @@ const Webapp = () => {
   return (
     <Box mt={{base: '100px'}} display='flex' flexDirection='column' alignItems="center" width="100%" maxWidth='1280px' pl='20px' pr='20px'>
         <Flex alignItems='center' justify={{base: 'space-evenly', md: 'space-evenly'}} flexWrap='wrap' width='100%'>
-            <Box as={Link} href="/#" flexDirection={{base: 'column', md: 'row'}} justifyContent='center' _hover={{textDecoration:'none'}} display='flex' alignItems='center' mr={{base: '0',md: '15px'}} background='#0088cc' padding='10px' borderRadius='5px'>
-                <Box className={glitch.menu} height="100%" width={{base: '40px', sm: '55px'}} pr={{base: '0', md:'10px'}}>
+            <Box as={Link} href="/#" flexDirection={{base: 'column', md: 'row'}} justifyContent='center' _hover={{textDecoration:'none'}} display='flex' alignItems='center' mr={{base: '0',md: '15px'}} background='#0088cc' padding='8px' borderRadius='5px'>
+                <Box className={glitch.menu} height="90%" width={{base: '35px', sm: '50px'}} pr={{base: '0', md:'10px'}}>
                     <FaTelegram />
                 </Box>
-                <Box display={{base: 'flex', md: 'block'}} flexDirection='column' justifyContent='center' alignItems='center' mt={{base: '10px', md: '0'}}>
-                    <Badge fontSize={{base: '10px', sm: '13px'}} p='5px' borderRadius='5px'>Available</Badge>
-                    <Box fontSize={{base: '14px', sm: '17', md: '20px'}} textTransform='uppercase' fontWeight='700' mt={{base: '10px', md: '0'}}>Telegram</Box>
+                <Box display={{base: 'flex', md: 'block'}} flexDirection='column' justifyContent='center' alignItems='center' mt={{base: '8px', md: '0'}}>
+                    <Badge fontSize={{base: '9px', sm: '12px'}} p='4px' borderRadius='5px'>Available</Badge>
+                    <Box fontSize={{base: '13px', sm: '16', md: '19px'}} textTransform='uppercase' fontWeight='700' mt={{base: '8px', md: '0'}}>Telegram</Box>
                 </Box>
             </Box>
             <Box display='flex' flexDirection={{base: 'column', md: 'row'}} justifyContent='center' alignItems='center' mr={{base: '0',md: '15px'}}>
