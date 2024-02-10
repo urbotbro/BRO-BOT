9dd607f0ee8b343206ae6a879dbee96606c48272
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-02-11 05:59:12 +0600

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,61 +17,84 @@ const Header = () => {
   const [state, changeState] = useState(false);
 
 
- return (
-    <Flex as='header'
-          justifyContent='space-between'
-          alignItems='center'
-          background='#0c0f12'
-          color='white'
-          position='fixed'
-          top='0'
-          left='0'
-          right='0'
-          width="100%"
-          zIndex='1000'
-          padding='10px'
-    >
-      {/* Logo */}
-      <Box>
-        <Image src='/Logo-min.png' alt="BRO" width={{ base: '60px', md: '80px' }} height={{ base: '60px', md: '80px' }} />
-      </Box>
+  return (
+    <Flex as='header' justifyContent='center' align='flex-start' background='#0c0f12' color='white' position='fixed' top='0' width="100%" zIndex='1000' pl='10px' pr='10px'>
+  <Flex as='div' position='sticky' maxWidth='1280px' width="100%" justify='space-between' align='center' paddingTop='20px' paddingBottom='20px' pl='20px' pr='20px'>
+     <Box>
+      <Image src='/Logo-min.png' alt="BRO" width={100} height={100} />
+    </Box>
 
-      {/* Rest of the header content */}
-      <Flex display={{ base: 'none', md: 'flex' }} alignItems='center'>
-        <Link href='#get-started' pl='10px' pr='10px' fontSize='15px' _hover={{ color: '#9bf0f5' }}>Getting Started</Link>
-        <Link href='#features' pl='10px' pr='10px' fontSize='15px' _hover={{ color: '#9bf0f5' }}>Features</Link>
-        <Link href='#roadmap' pl='10px' pr='10px' fontSize='15px' _hover={{ color: '#9bf0f5' }}>Roadmap</Link>
-        <Link href='#docs' pl='10px' pr='10px' fontSize='15px' _hover={{ color: '#9bf0f5' }}>Docs</Link>
+
+        <Box display={{base: 'none', md:'flex'}} justifyContent='space-between' alignItems='center'>
+          <Link href='#get-started' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{color: '#9bf0f5'}}>Getting Started</Link>
+          <Link href='#features' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{color: '#9bf0f5'}}>Features</Link>
+          <Link href='#roadmap' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{color: '#9bf0f5'}}>Roadmap</Link>
+          <Link href='/' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{color: '#9bf0f5'}}>Docs</Link>
+        </Box>
+        
+        <Box position='relative' display={{base: 'none', md: 'block'}}>
+          <Button borderRadius='100px' onClick={() => {toggleTool(!tool)}} style={orbitronFont.style} fontSize='14px' background='#cdf6f8' _hover={{
+              background: '#9bf0f5'
+            }}>
+              <Box as='span' paddingRight='7px'>Start</Box> <FaChevronDown />
+          </Button>
+          </Box>
+          <Box as='div' visibility={tool ? 'visible' : 'hidden' } zIndex={tool ? 1000 : 0} opacity={tool ? 1 : 0} transition={'all .2s ease-in-out'} flexDirection='column' minWidth='240px' background="#161c21" padding='10px' borderRadius='5px' right='10px' transform='translateY(85px)' position='absolute'>
+            <ChakraLink onClick={() => {toggleTool(false)}} as={Link} href='#' pt='10px' pb='10px' pl='5px' pr='5px' _hover={{textDecoration: 'none', background: '#1e262d'}} display='flex' alignItems='center'><Box className={glitch.menu} width='25px' height='25px'><FaTelegram /></Box> <Box as='span' ml='10px'>BRO BOT</Box></ChakraLink>
+          <Divider opacity='.1' />
+             <ChakraLink onClick={() => {toggleTool(false)}} as={Link} href='#' pt='10px' pb='10px' pl='5px' pr='5px' _hover={{textDecoration: 'none', background: '#1e262d'}} display='flex' alignItems='center'><Box className={glitch.menu} width='25px' height='25px'><FaTelegram /></Box> <Box as='span' ml='10px'>Signal BOT</Box></ChakraLink>
+          <Divider opacity='.1' />
+                  <Divider opacity='.1' />
+                <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{textDecoration: 'none',}} cursor='default' display='flex' alignItems='center'>
+                    BroTrade Hub <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Coming Soon</Badge>
+                  </Box>
+                 <Divider opacity='.1' />
+                <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{textDecoration: 'none',}} cursor='default' display='flex' alignItems='center'>
+                    Bro Launchpad <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Coming Soon</Badge>
+                  </Box>
+          </Box>
+        
+          <Box display={{base: 'flex', md: 'none'}} height="100%" width="27px" className={glitch.menu} justifyContent='center' alignItems='center' onClick={() => {changeState(!state)}}>
+            {state ? <FaXmark /> : <FaBars />}
+          </Box>
+
       </Flex>
-
-      {/* Mobile Menu Button */}
-      <Box display={{ base: 'flex', md: 'none' }} onClick={() => changeState(!state)}>
-        {state ? <FaXmark /> : <FaBars />}
+      <Box position='fixed' zIndex={state ? 10000 : 0} background="#0c0f12" top='0' bottom='0' left='0' right='0' display={{base: 'flex', md: 'none'}} visibility={state ? 'visible': 'hidden'} justifyContent='center' alignItems='flex-end' flexDirection='column'>
+        <Link mt='50px' mr='30px' opacity={state ? 1 : 0} onClick={() => {changeState(false)}} href='#get-started' pl='10px' pr='10px' fontSize='20px' style={orbitronFont.style} _hover={{color: '#9bf0f5'}}>Getting Started</Link>
+        <Link mt='50px' mr='30px' opacity={state ? 1 : 0} onClick={() => {changeState(false)}} href='#features' pl='10px' pr='10px' fontSize='20px' style={orbitronFont.style} _hover={{color: '#9bf0f5'}}>Features</Link>
+        <Link mt='50px' mr='30px' opacity={state ? 1 : 0} onClick={() => {changeState(false)}} href='#roadmap' pl='10px' pr='10px' fontSize='20px' style={orbitronFont.style} _hover={{color: '#9bf0f5'}}>Roadmap</Link>
+        <Link mt='50px' mr='30px' opacity={state ? 1 : 0} onClick={() => {changeState(false)}} href='#' pl='10px' pr='10px' fontSize='20px' style={orbitronFont.style} _hover={{color: '#9bf0f5'}}>Docs</Link>
+        <Box display={{base: 'flex', md: 'none'}} position='absolute' top='0' right='0' m='30px' width="35px" className={glitch.menu} justifyContent='center' alignItems='center' onClick={() => {changeState(false); toggleTool(false); toggleSmallTool(false)}}>
+            <FaXmark />
+        </Box>
+        <Box position='relative' display='block' width='100%' justifySelf='center' mt='50px'>
+          <Button borderRadius='0' minH='60px' display='flex' justifyContent='flex-end' alignItems='center' width='100%' onClick={() => {toggleSmallTool(!smallTool)}} style={orbitronFont.style} fontSize='20px' background='#cdf6f8' _hover={{
+              background: '#9bf0f5'
+            }}>
+              <Box mr="25px" display='flex' justifyContent='flex-end' alignItems='center'>
+              <Box as='span' paddingRight='7px'>Start</Box> <FaChevronDown />
+              </Box>
+          </Button>
+          </Box>
+          <Box as='div' visibility={smallTool ? 'visible' : 'hidden' } zIndex={smallTool ? 1000 : 0} opacity={smallTool ? 1 : 0} transition={'all .2s ease-in-out'} flexDirection='column' width='80%' background="#161c21" padding='10px' borderRadius='5px' right='10px' transform='translateY(286px)' position='absolute'>
+            <ChakraLink onClick={() => {toggleSmallTool(false)}} as={Link} href='#' pt='10px' pb='10px' pl='5px' pr='5px' _hover={{textDecoration: 'none', background: '#1e262d'}} display='flex' alignItems='center'><Box className={glitch.menu} width='25px' height='25px'><FaTelegram /></Box> <Box as='span' ml='10px'>BRO BOT</Box></ChakraLink>
+          <Divider opacity='.1' />
+    <ChakraLink onClick={() => {toggleTool(false)}} as={Link} href='#' pt='10px' pb='10px' pl='5px' pr='5px' _hover={{textDecoration: 'none', background: '#1e262d'}} display='flex' alignItems='center'>
+      <Box className={glitch.menu} width='25px' height='25px'><FaTelegram /></Box> 
+      <Box as='span' ml='10px'>Signal Bot</Box>
+    </ChakraLink>
+                  <Divider opacity='.1' />
+                <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{textDecoration: 'none',}} cursor='default' display='flex' alignItems='center'>
+                    BroTrade Hub <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Coming Soon</Badge>
+                  </Box>
+                 <Divider opacity='.1' />
+                <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{textDecoration: 'none',}} cursor='default' display='flex' alignItems='center'>
+                    Bro Launchpad <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Coming Soon</Badge>
+                  </Box>
+          </Box>
       </Box>
-
-      {/* Mobile Menu (Conditional Rendering based on 'state') */}
-      {state && (
-        <Flex
-          position='fixed'
-          background='#0c0f12'
-          width='100%'
-          height='100%'
-          top='0'
-          left='0'
-          zIndex='1000'
-          flexDirection='column'
-          justifyContent='center'
-          alignItems='center'
-        >
-          {/* Mobile Nav Links */}
-          <Link href='#get-started' fontSize='20px' _hover={{ color: '#9bf0f5' }}>Getting Started</Link>
-          <Link href='#features' fontSize='20px' _hover={{ color: '#9bf0f5' }}>Features</Link>
-          <Link href='#roadmap' fontSize='20px' _hover={{ color: '#9bf0f5' }}>Roadmap</Link>
-          <Link href='#docs' fontSize='20px' _hover={{ color: '#9bf0f5' }}>Docs</Link>
-        </Flex>
-      )}
     </Flex>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
