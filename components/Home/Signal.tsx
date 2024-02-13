07b04f3d20008fc8a27e85964d91e084bd574050
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-02-14 02:29:50 +0600

    Update Signal.tsx

--- components/Home/Signal.tsx
+++ components/Home/Signal.tsx
@@ -11,10 +11,10 @@ const Signal = () => {
     <Flex width="100%" maxWidth='1280px' justify='center' mt='150px' mb='150px' align='center'>
       <Flex width={{base: '90%', md: "90%"}} justify='space-between' direction={{base: 'column-reverse', md: 'row'}} textAlign={{base:'center', md: 'start'}} align='center' background='#fda007' padding={{base: '20px 20px', md: '30px 80px'}} borderRadius='10px'>
       <Box width={{base:'100%', md: '50%'}}>
-        <Heading pt='20px' pb='20px' fontSize={{base: '25px', sm: '30', md:'40px'}} style={orbitronFont.style} className={glitch.glitchWrapper}>
+        <Heading pt='20px' pb='20px' fontSize={{base: '25px', sm: '30', md:'40px'}} style={orbitronFont.style} className={glitch.glitchWrapper} color="black">
           <span className={glitch.glitch} data-text="Bro Signals">Bro Signals</span>
         </Heading>
-        <Box fontSize='20px'>Navigate the dynamic world of cryptocurrency with Bro Signals – the ultimate signal bot that transforms volatility into opportunity. Designed for both novice traders and seasoned investors, Bro Signals harnesses the power of advanced analytics to provide real-time trading alerts, empowering you to make informed decisions with confidence.</Box>
+        <Box fontSize='20px' color="black">Navigate the dynamic world of cryptocurrency with Bro Signals – the ultimate signal bot that transforms volatility into opportunity. Designed for both novice traders and seasoned investors, Bro Signals harnesses the power of advanced analytics to provide real-time trading alerts, empowering you to make informed decisions with confidence.</Box>
       </Box>
       <Flex width={{base:'100%', md: '50%'}} justify='center' align='center'>
         <Image src="/BRO SIGNAL BOT.png" width="450px" alt="Trade Like Never Before" />
