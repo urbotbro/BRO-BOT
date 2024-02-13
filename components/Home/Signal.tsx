5b20749f66c5a1343d9bf0696f0e98012e800738
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-02-13 16:00:39 +0600

    Update Signal.tsx

--- components/Home/Signal.tsx
+++ components/Home/Signal.tsx
@@ -17,7 +17,7 @@ const Signal = () => {
         <Box fontSize='20px'>Navigate the dynamic world of cryptocurrency with Bro Signals – the ultimate signal bot that transforms volatility into opportunity. Designed for both novice traders and seasoned investors, Bro Signals harnesses the power of advanced analytics to provide real-time trading alerts, empowering you to make informed decisions with confidence.</Box>
       </Box>
       <Flex width={{base:'100%', md: '50%'}} justify='center' align='center'>
-        <Image src="/gif2.gif" width="450px" alt="Trade Like Never Before" />
+        <Image src="/BRO SIGNAL BOT.png" width="450px" alt="Trade Like Never Before" />
       </Flex>
       </Flex>
     </Flex>
