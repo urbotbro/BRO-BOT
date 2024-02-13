526661befd7bc8eaa5cd5a94cba4df3ab8356284
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-02-14 03:21:17 +0600

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -35,28 +35,50 @@ interface roadmapList<type> {
   phases: phaseList<type>[]
 }
 
-export const Card = ({title, description, detail}: featureList<string>) => {
+export const FullLinkCard = (props: linkList) => {
+  // Mapping titles to their new button texts
+  const buttonTextMapping: { [key: string]: string } = {
+    'BRO Sniper Bot': 'Available Launch Day',
+    'BroTrade Hub': 'Coming Soon',
+    'Dashboards': 'Available on Launch Day',
+    'Bro Launchpads': 'Coming Soon',
+  };
+
+  // Determine the button text based on the card title
+  const buttonText = buttonTextMapping[props.title] || `Open ${props.title}`;
+
   return (
-    <ChakraCard minHeight={{base:'auto', md: '400px'}} background='#14191e' color='white' border='0.001px solid #ffffff23'>
-        <CardHeader>
-            <Heading size='md' mt='20px' mb='25px' style={orbitronFont.style}>
-              <span>{title}</span>
-            </Heading>
-        </CardHeader>
-        <CardBody>
-            <Box mb='40px' fontSize='20px' lineHeight='2'>
-              {description}
-              {detail ? 
-                <ul>
-                  {detail.map(elem => (<li key={detail.indexOf(elem)} style={{marginTop:'5px', fontSize: '18px'}}>&bull;{elem}</li>))}
-                </ul>
-              :
-              <></>}
-            </Box>
-        </CardBody>
+    <ChakraCard minH='650px' ml='20px' mr='20px' mb='20px' background='#14191e' color='white' border='0.001px solid #ffffff23'>
+      <CardBody>
+        <Box display='flex' position='relative' justifyContent='center' alignItems='center' width="100%" minHeight="200px">
+          <Box position='absolute' zIndex='10' display='flex' justifyContent='center' alignItems='center' width="100%" minHeight="200px" sx={{
+          background: `url('${props.imgSrc}') center no-repeat`,
+          backgroundSize: 'contain',
+          filter: props.comingSoon && 'blur(5px)',
+          }}></Box>
+          {props.comingSoon ? <Box as="span" zIndex="11" color="#ffffffab" style={orbitronFont.style}>Coming Soon</Box> : <></>}
+        </Box>
+        <Heading size='md' pt='30px' pb='30px' fontSize='30px' style={orbitronFont.style} className={glitch.glitchWrapper}>
+          <span className={glitch.glitch} data-text={props.title}>{props.title}</span>
+        </Heading>
+        <Box pb='20px' fontSize='18px'>
+          {props.description}
+        </Box>
+      </CardBody>
+      <CardFooter alignItems='center'>
+        {props.comingSoon ? (
+          <Box opacity='.4' display='flex' alignItems='center'> Coming Soon</Box>
+        ) : (
+          <Button as={Link} href={props.link} fontSize="14px" borderRadius='50px' background='#cdf6f8' _hover={{
+              background: '#9bf0f5'
+            }}>
+            {buttonText}
+          </Button>
+        )}
+      </CardFooter>
     </ChakraCard>
-  )
-}
+  );
+};
 
 export const ImageCard = (props: imageList) => {
   return (
