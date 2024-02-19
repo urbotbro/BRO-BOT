2b2dfbb70377a05f8d7ca9bef299d83544a8eefd
khushig8 <khushigarg.ai24@jecrc.ac.in>
2024-02-19 12:20:56 +0530

    Replace the web app section with a video and a button.

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import Signal from "@/components/Home/Signal";
 import Footer from "@/components/Footer";
 import { Box } from "@chakra-ui/react";
 import Boost from "@/components/Home/Boost";
-import Sliders from "@/components/Home/Slider";
-import Webapp2 from "@/components/Home/WebApp2";
+// import Sliders from "@/components/Home/Slider";
+import Webapp from "@/components/Home/Webapp";
 import GetStarted from "@/components/Home/GetStarted";
 import Loader from "@/components/Home/loader";
 import { useState, useEffect } from "react";
@@ -25,8 +25,8 @@ export default function Home() {
     <Header />
       <Box as="main" display='flex' alignItems='center' justifyContent='center' flexDirection='column'>
         <Intro />
-        <Sliders/>
-        {/* <Webapp2/> */}
+        {/* <Sliders/> */}
+        <Webapp/>
         <GetStarted />
         <Features />
         <Signal />
