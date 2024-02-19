a11b2bb2966c53dabb723b298d8afcbf4840708c
khushig8 <khushigarg.ai24@jecrc.ac.in>
2024-02-19 17:44:05 +0530

    height

--- components/Home/Webapp.tsx
+++ components/Home/Webapp.tsx
@@ -5,14 +5,10 @@ import glitch from '@/components/Home/glitch.module.css';
 const Webapp = () => {
   return (
     <Box mt={{ base: '100px' }} display='flex' flexDirection='column' alignItems="center" width="100%" maxWidth='1280px' pl='20px' pr='20px'>
-      <Flex mt='50px' flexDirection='column' alignItems="center" border='0.0001px solid #ffffff37' padding='20px' borderRadius='10px' position='relative' width="100%" minHeight={{ base: '300px', md: "600px" }}>
-        {/* <Box position='absolute' zIndex='10' display='flex' justifyContent='center' alignItems='center' width="100%" height="90%" sx={{
-          background: `url('/BROVideoFinal.gif') center`,
-          backgroundSize: 'cover', 
-          backgroundRepeat: 'no-repeat'
-        }}> */}
+      <Flex mt='50px' flexDirection='column' alignItems="center" border='0.0001px solid #ffffff37' padding='20px' borderRadius='10px' position='relative' width="100%" minHeight={{ base: '200px', md: "600px" }}>
+        
           <Image src="/BroVideoFinal.gif" alt="BROVideoFinal" borderRadius='10px' width='100%' />
-        {/* </Box> */}
+        
       </Flex>
       <Box display='flex' alignItems='center' justifyContent='center' pt='30px' pb='20px' fontSize='18px'>
                 <span className={glitch.glitch}>For More Visit </span>
