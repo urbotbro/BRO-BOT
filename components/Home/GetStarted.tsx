572a4cc7e364475b557299c6c2d1cc63c79247dd
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-03-16 04:05:34 +0600

    Update GetStarted.tsx

--- components/Home/GetStarted.tsx
+++ components/Home/GetStarted.tsx
@@ -24,7 +24,7 @@ const GetStarted = () => {
             comingSoon: false,
             description: 'Token holders can seize a portion of revenue by utilizing this dashboard. Furthermore, you have the option to generate a referral link and collect rewards for successful referrals.',
             imgSrc: '/BRO BOT Dash.PNG',
-            link: 'https://dashboard.itsurbro.com/',
+            link: 'https://revshare.itsurbro.com/',
         },
         {
             title: 'Bro Launchpads',
