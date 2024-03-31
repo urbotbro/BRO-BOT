75f06ca3b6a7238d4fb8cf557f699367356e37b4
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-04-01 02:33:46 +0600

    Update CountdownTimer.tsx

--- components/Home/CountdownTimer.tsx
+++ components/Home/CountdownTimer.tsx
@@ -90,8 +90,8 @@ const CountdownTimer: React.FC = () => {
               href="https://www.pinksale.finance/launchpad/0x696d9fDe0ad616fd463E5c5D2c67F75f8D7c8F22?chain=ETH&refId=0x37950C488Cd8f0f58AA661B7560D1Ba03a608b93"
               target="_blank"
               fontSize="18px"
-              background="#fda007"
-              _hover={{ background: "#fda007" }}
+              background="#FFB6C1"
+              _hover={{ background: "#FFB6C1" }}
               rel="noopener noreferrer"
             >
               Presale
