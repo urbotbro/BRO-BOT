22db42d904c4ff1f27f37aa717704da8e1b85cb4
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-03-31 23:48:35 +0600

    Update Webapp.tsx

--- components/Home/Webapp.tsx
+++ components/Home/Webapp.tsx
@@ -1,34 +1,35 @@
-import { Box, Flex, Image, Button } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Flex, Button } from "@chakra-ui/react";
+import NextLink from "next/link";
 import glitch from '@/components/Home/glitch.module.css';
 
 const Webapp = () => {
   return (
     <Box mt={{ base: '100px' }} display='flex' flexDirection='column' alignItems="center" width="100%" maxWidth='1280px' pl='20px' pr='20px'>
-      <Flex mt='50px' flexDirection='column' alignItems="center" border='0.0001px solid #ffffff37' padding='20px' borderRadius='10px' position='relative' width="100%" minHeight={{ base: '200px', md: "600px" }}>
-        
-         <video width="100%" borderRadius="10px" controls>
-  <source src="/bropresale.mp4" type="video/mp4" />
-</video>
-
-        
+      <Flex mt='50px' flexDirection='column' alignItems="center" padding='20px' borderRadius='10px' position='relative' width="100%" minHeight={{ base: '200px', md: "600px" }}>
+        {/* Video container with Chakra UI styling */}
+        <Box width="100%" borderRadius="10px" overflow="hidden">
+          <video width="100%" controls>
+            <source src="/bropresale.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </Box>
       </Flex>
       <Box display='flex' alignItems='center' justifyContent='center' pt='30px' pb='20px' fontSize='18px'>
-         <span className={glitch.glitch}>For More Visit </span>
-         <Button
-           as={Link}
-           href="https://tradewithbro.com"
-           fontSize="18px"
-           background="#fda007"
-           _hover={{
-              background: '#fda007'
-           }}
-         >
-          TradeWithBro
-        </Button> 
+        <span className={glitch.glitch}>For More Visit </span>
+        <NextLink href="https://tradewithbro.com" passHref>
+          <Button
+            as="a" // Use `a` tag to properly redirect
+            fontSize="18px"
+            background="#fda007"
+            _hover={{
+              background: '#fda007',
+            }}
+          >
+            TradeWithBro
+          </Button>
+        </NextLink>
      </Box>
-</Box>
-
+    </Box>
   );
 };
 
