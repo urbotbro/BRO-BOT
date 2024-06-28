09904c360fb52277831c73adc4af82368e5fd226
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-06-28 15:48:25 +0600

    Update loader.tsx

--- components/Home/loader.tsx
+++ components/Home/loader.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Loader = ({ visibility }: {visibility: boolean}) => {
   return (
     <Box visibility={visibility ? 'visible': 'hidden'} transition='all .3s ease-in-out' display='flex' background='#0c0f12' color='white' zIndex={visibility ? '10000': '-10'} position='fixed' top='0' left='0' right='0' bottom='0'  justifyContent='center' alignItems='center'>
-        <Image src="/pandaloader.gif" alt='Site Loading' />
+        <Image src="/loader.gif" alt='Site Loading' />
     </Box>
   )
 }
