8aee111e698686513b1b888d27b2e95beb921df2
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-06-28 16:02:00 +0600

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,56 +1,67 @@
 'use client';
-import { Link as ChakraLink, Divider, Box, Button, Flex, Popover, PopoverBody, Badge, PopoverFooter, PopoverContent, PopoverHeader, PopoverTrigger, Portal } from '@chakra-ui/react';
+import { Link as ChakraLink, Divider, Box, Button, Flex, Popover, PopoverBody, Badge, PopoverFooter, PopoverContent, PopoverHeader, PopoverTrigger, Portal } from '@chakra-ui/react'
 import React, { useState } from 'react';
 import { Link, Image } from '@chakra-ui/next-js';
-import { Ubuntu, Orbitron } from 'next/font/google';
+import { Ubuntu, Orbitron } from "next/font/google";
 import { FaBars, FaXmark, FaTelegram } from 'react-icons/fa6';
-import { FaChevronDown } from 'react-icons/fa';
-import glitch from '@/components/Home/glitch.module.css';
+import { FaChevronDown } from "react-icons/fa";
+import glitch from '@/components/Home/glitch.module.css'
 import tooltip from '@/components/Home/tooltip.module.css';
 import Loader from './Home/loader';
-
-const ubuntuFont = Ubuntu({ weight: ['300', '400', '500', '700'], subsets: ['latin'] });
-const orbitronFont = Orbitron({ weight: ['400', '500', '600', '700'], subsets: ['latin'] });
+const ubuntuFont = Ubuntu({ weight: ['300', '400', '500', '700'], subsets: ['latin'] })
+const orbitronFont = Orbitron({ weight: ['400', '500', '600', '700'], subsets: ['latin'] })
 
 const Header = () => {
   const [tool, toggleTool] = useState(false);
   const [smallTool, toggleSmallTool] = useState(false);
   const [state, changeState] = useState(false);
 
+
   return (
     <Flex as='header' justifyContent='space-between' align='center' color='white' position='fixed' top='0' width="100%" zIndex='1000' pl='5px' pr='5px'>
       <Flex as='div' position='sticky' maxWidth='2280px' width="100%" justify='space-between' align='center' paddingTop='10px' paddingBottom='10px' pl='5px' pr='5px'>
         <Box>
-          <Image src='/panda.png' alt="BRO" width={95} height={95} />
+          <Image src='/Logo-min.png' alt="BRO" width={95} height={95} />
         </Box>
+
 
         <Box display={{ base: 'none', md: 'flex' }} justifyContent='space-between' alignItems='center'>
           <Link href='#get-started' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{ color: '#9bf0f5' }}>Getting Started</Link>
           <Link href='#features' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{ color: '#9bf0f5' }}>Features</Link>
           <Link href='#roadmap' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{ color: '#9bf0f5' }}>Roadmap</Link>
-          <Link href='https://bro-bot-academy.gitbook.io/brobot' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{ color: '#9bf0f5' }} target="_blank">Tutorial</Link>
-          <Link href='/BRO Whitepaper.pdf' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{ color: '#9bf0f5' }} target="_blank'>Whitepaper</Link>
+          <Link href='https://bro-bot-academy.gitbook.io/brobot' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{ color: '#9bf0f5' }} target="_blank">Docs</Link>
+          <Link href='/BRO Whitepaper.pdf' pl='10px' pr='10px' fontSize='15px' style={orbitronFont.style} _hover={{ color: '#9bf0f5' }} target="_blank">Whitepaper</Link>
         </Box>
 
         <Box position='relative' display={{ base: 'none', md: 'block' }}>
-          <Button borderRadius='100px' onClick={() => { toggleTool(!tool) }} style={orbitronFont.style} fontSize='14px' background='#fda007' _hover={{ background: '#fda007' }}>
+          <Button borderRadius='100px' onClick={() => { toggleTool(!tool) }} style={orbitronFont.style} fontSize='14px' background='#fda007' _hover={{
+            background: '#fda007'
+          }}>
             <Box as='span' paddingRight='7px'>Start</Box> <FaChevronDown />
           </Button>
         </Box>
         <Box as='div' visibility={tool ? 'visible' : 'hidden'} zIndex={tool ? 1000 : 0} opacity={tool ? 1 : 0} transition={'all .2s ease-in-out'} flexDirection='column' minWidth='240px' background="#161c21" padding='10px' borderRadius='5px' right='10px' transform='translateY(85px)' position='absolute'>
-          <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{ textDecoration: 'none' }} cursor='pointer' display='flex' alignItems='center'>
-            <a href="" target="_blank" rel="noopener noreferrer" className="sniperBotLink" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
-              <FaTelegram style={{ marginRight: '10px' }} />
-              Sniper Bot
-            </a>
-          </Box>
+          {/* <ChakraLink onClick={() => {toggleTool(false)}} as={Link} href='#' pt='10px' pb='10px' pl='5px' pr='5px' _hover={{textDecoration: 'none', background: '#1e262d'}} display='flex' alignItems='center'><Box className={glitch.menu} width='25px' height='25px'><FaTelegram /></Box> <Box as='span' ml='10px'>BRO BOT</Box></ChakraLink>
+            <ChakraLink onClick={() => {toggleTool(false)}} as={Link} href='#' pt='10px' pb='10px' pl='5px' pr='5px' _hover={{textDecoration: 'none', background: '#1e262d'}} display='flex' alignItems='center'><Box className={glitch.menu} width='25px' height='25px'><FaTelegram /></Box> <Box as='span' ml='10px'>BRO SNIPER BOT</Box></ChakraLink>
           <Divider opacity='.1' />
+             <ChakraLink onClick={() => {toggleTool(false)}} as={Link} href='#' pt='10px' pb='10px' pl='5px' pr='5px' _hover={{textDecoration: 'none', background: '#1e262d'}} display='flex' alignItems='center'><Box className={glitch.menu} width='25px' height='25px'><FaTelegram /></Box> <Box as='span' ml='10px'>Signal BOT</Box></ChakraLink> */}
+          {/* <Divider opacity='.1' />
+            <ChakraLink onClick={() => {toggleTool(false)}} as={Link} href='https://tradewithbro.com' pt='10px' pb='10px' pl='5px' pr='5px' _hover={{textDecoration: 'none', background: '#1e262d'}} display='flex' alignItems='center'><Box className={glitch.menu} width='25px' height='25px'><FaTelegram /></Box> <Box as='span' ml='10px'>BroTrade Hub</Box></ChakraLink>
+          <Divider opacity='.1' />
+             <ChakraLink onClick={() => {toggleTool(false)}} as={Link} href='https://tradewithbro.com/pages/launch.html' pt='10px' pb='10px' pl='5px' pr='5px' _hover={{textDecoration: 'none', background: '#1e262d'}} display='flex' alignItems='center'><Box className={glitch.menu} width='25px' height='25px'><FaTelegram /></Box> <Box as='span' ml='10px'>Bro Launchpad</Box></ChakraLink> */}
           <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{ textDecoration: 'none' }} cursor='pointer' display='flex' alignItems='center'>
-            <a href="" target="_blank" rel="noopener noreferrer" className="signalBotLink" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
-              <FaTelegram style={{ marginRight: '10px' }} />
-              Signal Bot
-            </a>
-          </Box>
+             <a href="https://t.me/snipewithbro_bot" target="_blank" rel="noopener noreferrer" className="sniperBotLink" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
+                <FaTelegram style={{ marginRight: '10px' }} />
+                Sniper Bot
+              </a>
+            </Box>
+            <Divider opacity='.1' />
+            <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{ textDecoration: 'none' }} cursor='pointer' display='flex' alignItems='center'>
+              <a href="https://t.me/brosignal_bot" target="_blank" rel="noopener noreferrer" className="signalBotLink" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
+                 <FaTelegram style={{ marginRight: '10px' }} />
+                 Signal Bot
+               </a>
+             </Box>
           <Divider opacity='.1' />
           <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{ textDecoration: 'none', }} cursor='default' display='flex' alignItems='center'>
             BroTrade Hub <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Soon</Badge>
@@ -76,35 +87,38 @@ const Header = () => {
           <FaXmark />
         </Box>
         <Box position='relative' display='block' width='100%' justifySelf='center' mt='50px'>
-          <Button borderRadius='0' minH='60px' display='flex' justifyContent='flex-end' alignItems='center' width='100%' onClick={() => { toggleSmallTool(!smallTool) }} style={orbitronFont.style} fontSize='20px' background='#fda007' _hover={{ background: '#fda007' }}>
+          <Button borderRadius='0' minH='60px' display='flex' justifyContent='flex-end' alignItems='center' width='100%' onClick={() => { toggleSmallTool(!smallTool) }} style={orbitronFont.style} fontSize='20px' background='#fda007' _hover={{
+            background: '#fda007'
+          }}>
             <Box mr="25px" display='flex' justifyContent='flex-end' alignItems='center'>
               <Box as='span' paddingRight='7px'>Start</Box> <FaChevronDown />
             </Box>
           </Button>
         </Box>
         <Box as='div' visibility={smallTool ? 'visible' : 'hidden'} zIndex={smallTool ? 1000 : 0} opacity={smallTool ? 1 : 0} transition={'all .2s ease-in-out'} flexDirection='column' width='80%' background="#161c21" padding='10px' borderRadius='5px' right='10px' transform='translateY(286px)' position='absolute'>
-          <ChakraLink onClick={() => toggleSmallTool(false)} href='' isExternal pt='10px' pb='10px' pl='5px' pr='5px' _hover={{ textDecoration: 'none', background: '#1e262d' }} display='flex' alignItems='center'>
-            <Box width='25px' height='25px'><FaTelegram /></Box>
-            <Box as='span' ml='10px'>BRO SNIPER BOT</Box>
-          </ChakraLink>
-          <Divider opacity='.1' />
-          <ChakraLink onClick={() => toggleTool(false)} href='' isExternal pt='10px' pb='10px' pl='5px' pr='5px' _hover={{ textDecoration: 'none', background: '#1e262d' }} display='flex' alignItems='center'>
-            <Box width='25px' height='25px'><FaTelegram /></Box>
-            <Box as='span' ml='10px'>Signal Bot</Box>
-            <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Soon</Badge>
-          </ChakraLink>
+             <ChakraLink onClick={() => toggleSmallTool(false)} href='https://t.me/snipewithbro_bot' isExternal pt='10px' pb='10px' pl='5px' pr='5px' _hover={{ textDecoration: 'none', background: '#1e262d' }} display='flex' alignItems='center'>
+                <Box width='25px' height='25px'><FaTelegram /></Box>
+                <Box as='span' ml='10px'>BRO SNIPER BOT</Box>
+             </ChakraLink>
+             <Divider opacity='.1' />
+             <ChakraLink onClick={() => toggleTool(false)} href='https://t.me/brosignal_bot' isExternal pt='10px' pb='10px' pl='5px' pr='5px' _hover={{ textDecoration: 'none', background: '#1e262d' }} display='flex' alignItems='center'>
+                <Box width='25px' height='25px'><FaTelegram /></Box>
+                <Box as='span' ml='10px'>Signal Bot</Box>
+                <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Coming Soon</Badge>
+            </Box>
+             </ChakraLink>
           <Divider opacity='.1' />
           <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{ textDecoration: 'none', }} cursor='default' display='flex' alignItems='center'>
-            BroTrade Hub <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Soon</Badge>
+            BroTrade Hub <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Coming Soon</Badge>
           </Box>
           <Divider opacity='.1' />
           <Box pt='10px' pb='10px' pl='5px' pr='5px' opacity='0.7' _hover={{ textDecoration: 'none', }} cursor='default' display='flex' alignItems='center'>
-            Bro Launchpad <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Soon</Badge>
+            Bro Launchpad <Badge variant='outline' justifySelf='flex-end' alignItems='center' display='flex' justifyContent='center' alignContent='center' ml='10px' colorScheme='green'>Coming Soon</Badge>
           </Box>
         </Box>
       </Box>
     </Flex>
-  );
-};
+  )
+}
 
 export default Header;
