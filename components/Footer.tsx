7e29c85c56b3d5a5a06cffd704daf56340434c5d
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-06-29 19:24:12 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,10 +26,10 @@ const Footer = () => {
         
         <Box>
           <Flex justify='center' align='center' transition='all 0.2s ease-in-out'>
-            <Link href='' target="_blank" width='30px' height='30px' ml='10px' mr='10px' display='flex'justifyContent='center' alignItems="center" className={glitch.div}>
+            <Link href='https://x.com/PandaBRO___' target="_blank" width='30px' height='30px' ml='10px' mr='10px' display='flex'justifyContent='center' alignItems="center" className={glitch.div}>
               <FaXTwitter />
             </Link>
-            <Link href="" target="_blank" width='30px' height='30px' ml='10px' mr='10px' display='flex'justifyContent='center' alignItems="center" className={glitch.div}>
+            <Link href="https://t.me/pandabro_portal" target="_blank" width='30px' height='30px' ml='10px' mr='10px' display='flex'justifyContent='center' alignItems="center" className={glitch.div}>
               <FaTelegram />
             </Link>
           </Flex>
