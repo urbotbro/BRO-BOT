5fef3317e17c8e80c9cbdaab516e8163e105825a
urbotbro <150325977+urbotbro@users.noreply.github.com>
2024-06-30 00:12:30 +0600

    Update TokenomicsChart.tsx

--- components/Home/TokenomicsChart.tsx
+++ components/Home/TokenomicsChart.tsx
@@ -20,7 +20,7 @@ const centerTextPlugin = {
     ctx.fillStyle = "rgb(253, 160, 7)";
 
     const text = "$BRO";
-    const textX = centerX - width / 3; 
+    const textX = centerX - width / 2; 
     const textY = centerY;
 
     ctx.fillText(text, textX, textY);
